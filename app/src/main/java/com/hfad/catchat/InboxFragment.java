64df174f9d7ac96513e56a3369d237cceaa968dc
DheoGV <dheogildasvarian@gmail.com>
2020-10-31 00:00:34 +0700

    Initial commit

---
+++ app/src/main/java/com/hfad/catchat/InboxFragment.java
@@ -0,0 +1,21 @@
+package com.hfad.catchat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InboxFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InboxFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
+    }
+}
